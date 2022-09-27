279a2e41db5d446781cf8e5aadaec9c978cf88a6
lukejlackey <lukejlackey@gmail.com>
2022-09-27 12:53:15 -0700

    updated gitignore

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,23 @@
+import logging
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask
 from flask_socketio import SocketIO, join_room, leave_room, emit
 from classes.active_games import active_games
 
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 app = Flask(__name__)
-app.secret_key = 'lM@0h@x0R'
+app.secret_key = os.environ.get("SECRET_KEY")
 
 socketio = SocketIO(app, manage_session=False)
 
-DATABASE = 'blackjackDB'
+DATABASE = os.environ.get("DATABASE")
 
 active_players = []
 players_in_queue = []
